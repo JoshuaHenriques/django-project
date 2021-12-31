6a5cc8ffc58221d1f516fc704519a277a0df3283
joshuahenriques <joshuahenriques95@gmail.com>
2021-12-31 14:14:45 -0500

    extend html pages

--- board/urls.py
+++ board/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.index), # our-domain.com/
-	path('post/<slug:post_id>', views.post_details) # our-domain.com/<dynamic>
+	path('', views.index, name='all-posts'), # our-domain.com/
+	path('post/<slug:post_id>', views.post_details, name='post-detail') # our-domain.com/<dynamic>
 ]
